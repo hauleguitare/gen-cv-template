d5a4b17f8f29f88a58969311cf868fe0074b86af
hauleguitare <alexander.dutyone@gmail.com>
2023-02-04 12:05:40 +0700

    update: render children, primitive HTML

--- src/component-handler/index.tsx
+++ src/component-handler/index.tsx
@@ -1,16 +1,42 @@
 import React from "react";
-import {IAttributeComponent} from "../../../gen-dynamic-cv/src/types/attributes-component";
 import getComponent from "./component";
+import {IAttributeComponent} from "../types/attributes-component";
 
-export default function RenderComponentHandler(attributes: IAttributeComponent): any {
+export default function renderComponentHandler(attributes: IAttributeComponent): any {
+    // Check if lower-case will be render primitive html element, Be Careful
+    if (attributes.component.charAt(0) === attributes.component.charAt(0).toLowerCase()){
+        return React.createElement(attributes.component, {
+                key: attributes._uid,
+                ...attributes.props,
+            },
+            renderComponentChildren(attributes.children)
+        )
+    }
+
     const component = getComponent(attributes.component);
     if (typeof component !== 'undefined'){
+        // Render custom Component
         return React.createElement(component,
             {
                 key: attributes._uid,
                 ...attributes.props
             },
-            attributes.children && attributes.children.map((c) => RenderComponentHandler(c))
+            renderComponentChildren(attributes.children)
         )
     }
+}
+
+
+function renderComponentChildren(children:  IAttributeComponent[] | null | undefined | string) {
+    if (children){
+        if (Array.isArray(children)){
+            return children.map((comp) => renderComponentHandler(comp))
+        }
+
+        if (typeof children === 'string'){
+            return children
+        }
+    }
+
+
 }
