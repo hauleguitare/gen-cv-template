b375b33107bb6cc706458d95b731812552e30263
hauleguitare <alexander.dutyone@gmail.com>
2023-02-04 12:08:29 +0700

    update: adding data file container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {IAttributeComponent} from "./types/attributes-component";
+import {ulid} from "ulid";
+import renderComponentHandler from "./component-handler";
+
+const data: IAttributeComponent[] = [
+  {
+    _uid: ulid(),
+    component: 'Container',
+    props: {
+      className: 'py-4 px-4',
+    },
+    children: [
+      {
+        _uid: ulid(),
+        component: 'div',
+        children: 'Hello world',
+        props: {
+        }
+      }
+    ]
+  }
+]
+
 
 function App() {
   return (
     <div>
-
+      {data.map((component) => renderComponentHandler(component))}
     </div>
   );
 }
